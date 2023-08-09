dcccd88cb22db334d54f63814f576f6fda236cb7
Alejandra Lopez <mlopezalejandra13@gmail.com>
2023-08-08 21:20:33 -0500

    multiplicación

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("MULTIPLICACIÓN");
+
+// Paso a paso
+alert("Vamos a empezar");
+var numeroA = prompt("Digite el primer numero");
+var numeroB = prompt(" Digite el segundo numero");
+
+// Convertir cadenas a enteros usando parseInt
+numeroA = parseInt(numeroA);
+numeroB = parseInt(numeroB);
+
+var resultado = numeroA * numeroB;
+
+// Mostrar el resultado usando la función alert
+
+alert("El resultado es: " + resultado);
